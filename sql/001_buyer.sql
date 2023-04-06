9338e03b4152b31f3564062b528db431e4299cba
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-04-06 18:31:02 +0700

    fix(OPS): Add dockerfile and docker compose

--- sql/001_buyer.sql
+++ sql/001_buyer.sql
@@ -5,3 +5,7 @@ CREATE TABLE IF NOT EXISTS buyer (
   password VARCHAR(255) NOT NULL,
   alamat_pengiriman VARCHAR(255) NOT NULL
 );
+
+
+INSERT INTO buyer (email, name, password, alamat_pengiriman)
+VALUES ('user@mail.com', 'John Doe', '$2a$10$xCniCdjZENytpQ7/NTNQduSJaZ6pl3bFMbd7bfF4OLwwbCyhGH8rC', '123 Main St');
